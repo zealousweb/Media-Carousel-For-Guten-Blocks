c0fa9e565b9860bc6f8c0fd1b6f30c170537975a
Maitri Prajapti <maitri@zealousweb.com>
2024-04-24 10:25:06 +0530

    make slider type default

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'd24dc22fe3113da9e8b6');
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '413eb9fd8f50e1963e2a');
