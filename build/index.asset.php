9823a959d902b3b8fe72f9f40a16a56128bf9be4
Maitri Prajapti <maitri@zealousweb.com>
2024-05-29 19:12:42 +0530

    width css

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '28e6f35ea74bbd3ac0bf');
+<?php return array('dependencies' => array('jquery', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6bef28e088106d441e47');
